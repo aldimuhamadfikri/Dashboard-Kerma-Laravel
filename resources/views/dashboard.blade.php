c568e75195efa0b50694d790d0f70ab9c18901eb
Aldi <aldimuhamadfikri@gmail.com>
2023-06-15 10:21:47 +0700

    upload

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('title','Data Barang')
+
+@section('contents')
+<!-- DataTales Example -->
+<div class="card shadow mb-4">
+    <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">Data Barang</h6>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Kode Barang</th>
+                        <th>Nama Barang</th>
+                        <th>Kategori</th>
+                        <th>Harga</th>
+                        <th>Jumlah</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
+</div>
+
+@endsection
